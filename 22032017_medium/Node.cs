a9af9bcedfec23e76f99dbf2a3b1c1fd229775f2
ogendogen <kosmos500@interia.pl>
2017-03-28 21:14:55 +0200

    Commit
    Commit

---
+++ 22032017_medium/Node.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22032017_medium
+{
+    class Node<T>
+    {
+        private T data;
+        private List<Node<T>> children;
+        private Node<T> parent;
+
+        public Node(T data, int planned_children = 2, Node<T> parent = null)
+        {
+            this.data = data;
+            this.parent = parent;
+            children = new List<Node<T>>(planned_children);
+        }
+
+        public void setParent(Node<T> parent)
+        {
+            if (parent == null) throw new NullReferenceException("Null given, node expected");
+            this.parent = parent;
+        }
+
+        public Node<T> getParent()
+        {
+            return parent;
+        }
+
+        public void addChild(Node<T> child)
+        {
+            if (child == null) throw new NullReferenceException("Null given, node expected");
+            children.Add(child);
+        }
+
+        public List<Node<T>> getChildren()
+        {
+            return children;
+        }
+
+        public T getData()
+        {
+            return data;
+        }
+
+        public bool isRoot()
+        {
+            if (parent == null) return true;
+            return false;
+        }
+
+        public bool isLeaf()
+        {
+            if (children.Count() == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ 22032017_medium/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _22032017_medium
+{
+    class Program
+    {
+        private static string[] lines = File.ReadAllLines("enable1.txt");
+        private static string longest = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj frazę");
+            string letters = Console.ReadLine();
+
+            Node<string> root = new Node<string>(letters); // korzeń
+            buildTree(root);
+            findAnswer(root);
+
+            Console.WriteLine("Wynik: " + longest);
+            Console.ReadKey();
+        }
+
+        private static void buildTree(Node<string> parent)
+        {
+            string data = parent.getData();
+            foreach (var line in lines) // dla każdej znalezionej możliwości, dodawaj nowy węzeł
+            {
+                if (line.Length == data.Length + 1 && line.Contains(data))
+                {
+                    try
+                    {
+                        parent.addChild(new Node<string>(line));
+                    }
+                    catch(NullReferenceException)
+                    {
+                        Console.WriteLine("Empty child");
+                    }
+                }
+            }
+
+            if (parent.isLeaf()) return; // jeśli liść to przerwij
+            var children = parent.getChildren();
+            foreach (var child in children) buildTree(child);
+        }
+
+        private static void findAnswer(Node<string> parent)
+        {
+            if (parent.getData().Length > longest.Length) longest = parent.getData();
+            if (parent.isLeaf()) return; // jeśli liść to przerwij
+            var children = parent.getChildren();
+            foreach (var child in children) findAnswer(child);
+        }
+    }
+}
